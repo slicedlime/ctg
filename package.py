7f449c05715f6735bc44e256028b9f82b3364e07
slicedlime <slicedlime@gmail.com>
2022-04-18 00:12:31 +0200

    Add resources processing and upgrade output to packaging

--- package.py
+++ package.py
@@ -3,6 +3,7 @@ from operator import le
 import os
 from distutils import dir_util
 import shutil
+from struct import pack
 from sys import prefix
 from zipfile import ZipFile
 
@@ -39,6 +40,12 @@ def strip_lesson_folders(parent_folder, lesson):
         folder_lesson = int(sfolder[6:])
         if folder_lesson > lesson:
             shutil.rmtree(parent_folder + '/' + sfolder)
+
+def mirror_dir(name, folder):
+    dir_util.copy_tree(name,  folder + '/' + name)
+
+def mirror_file(name, folder):
+    shutil.copyfile(name, folder + '/' + name)
 
 def make_datapack(folder, lesson):
     print('Making datapack for lesson ' + str(lesson))
@@ -80,13 +87,22 @@ def make_datapack(folder, lesson):
     zip(pack_file, folder + '/ctg', ['ctg.zip'])
     return pack_file
 
-def mirror_dir(name, folder):
-    dir_util.copy_tree(name,  folder + '/' + name)
+def make_resourcepack(folder, lesson):
+    print('Making datapack for lesson ' + str(lesson))
+    # Copy folder
+    dir_util.copy_tree('resourcepacks/ctg', folder + '/ctg')
+    pack_file = folder + '/resources.zip'
+    zip(pack_file, folder + '/ctg', ['resources.zip'])
+    return pack_file
 
-def mirror_file(name, folder):
-    shutil.copyfile(name, folder + '/' + name)
+def copy_packs(folder, pack_path, resource_zip_path):
+    # Datapack
+    os.makedirs(folder + '/datapacks', exist_ok=True)
+    shutil.copy(pack_path, folder + '/datapacks/ctg.zip')
+    # Resource pack
+    shutil.copy(resource_zip_path, folder + '/resources.zip')
 
-def make_world(folder, pack_path, output_path, lesson):
+def make_world(folder, pack_path, resource_zip_path, output_path, lesson):
     print('Packaging world for lesson ' + str(lesson))
     os.makedirs(folder, exist_ok=True)
     # World data
@@ -96,9 +112,8 @@ def make_world(folder, pack_path, output_path, lesson):
     # Metadata
     os.makedirs(folder + '/data', exist_ok=True)
     mirror_file('data/scoreboard.dat', folder)
-    # Datapack
-    os.makedirs(folder + '/datapacks', exist_ok=True)
-    shutil.copy(pack_path, folder + '/datapacks/ctg.zip')
+    # Packs
+    copy_packs(folder, pack_path, resource_zip_path)
     # Top-level files
     mirror_file('icon.png', folder)
     mirror_file('level.dat', folder)
@@ -107,17 +122,32 @@ def make_world(folder, pack_path, output_path, lesson):
     zip(zip_path, folder)
     return zip_path
 
+def make_upgrade_zip(folder, pack_path, resource_zip_path, output_path):
+    os.makedirs(folder, exist_ok=True)
+    copy_packs(folder, pack_path, resource_zip_path)
+    # Create level zip
+    zip_path = output_path + '/Upgrade.zip'
+    zip(zip_path, folder)
+    return zip_path
+
 def package_lesson(end_lesson):
     lesson_folder = BUILD_PREFIX + 'Lesson%02d' % end_lesson
+    shutil.rmtree(lesson_folder)
     pack_folder = lesson_folder + '/Pack'
     pack_path = make_datapack(pack_folder, end_lesson)
 
+    resource_folder = lesson_folder + '/Resources'
+    resource_zip_path = make_resourcepack(resource_folder, end_lesson)
+
     world_folder = lesson_folder + '/CommandTrainingGrounds'
-    world_zip_path = make_world(world_folder, pack_path, lesson_folder, end_lesson)
+    upgrade_zip_path = make_upgrade_zip(world_folder, pack_path, resource_zip_path, lesson_folder)
+    world_zip_path = make_world(world_folder, pack_path, resource_zip_path, lesson_folder, end_lesson)
 
     output_path = OUTPUT_PREFIX + 'Lesson%02d' % end_lesson
-    os.makedirs(output_path)
+    os.makedirs(output_path, exist_ok=True)
     shutil.copyfile(pack_path, output_path + '/ctg.zip')
+    shutil.copyfile(resource_zip_path, output_path + '/resources.zip')
+    shutil.copyfile(upgrade_zip_path, output_path + '/Upgrade.zip')
     shutil.copyfile(world_zip_path, output_path + '/CommandTrainingGrounds.zip')
 
 lesson_count = 2
